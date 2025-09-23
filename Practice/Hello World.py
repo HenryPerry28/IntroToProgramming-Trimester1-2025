ecfac95b5c2a32c5e2be79f37e64321116c2d238
Perry Henry <825834@stma.k12.mn.us>
2025-09-23 13:51:06 -0500

    Initial Commit

---
+++ Practice/Hello World.py
@@ -0,0 +1,27 @@
+#Data Types
+#Boolean - True of false values
+#Charaters - Single keyboard symbol
+#String - A line of characters
+#Practice
+
+
+name = "Henry"
+age1 = 15
+age2 = 17
+age3 = 16
+age4 = 16
+x = 3
+y = 7
+z = x * y
+sum = x + y
+
+print("Hello World!")
+print("Hello", name)
+print("Name:", name, "Age:", age1)
+
+print ("x:", x)
+print ("y:", y)
+print ("z:", z)
+print ("Sum:", sum)
+
+print("Average age of some people I know:", (age1 + age2 + age3 + age4) / 4)
