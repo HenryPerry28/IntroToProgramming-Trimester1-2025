4676106dfcb1a8fba51658b41d41cc475473079f
Perry Henry <825834@stma.k12.mn.us>
2025-09-30 13:47:43 -0500

    Mad Lib

---
+++ Assignments/SimpleMadLib.py
@@ -0,0 +1,22 @@
+'''
+There was once a person who lived in a [adjective1] spire.
+[pronoun1] was a very [adjective2] person, and their name was [name1]
+They liked to do [adverb1] [verb1].
+However, the [adjective2] person did have a [creature1]
+And they liked to play with their [creature1], but it was very [adjective 3] and like to [verb2] very [adverb2]
+So [name1] had to leave their [adjective1] spire in search of a better home for their [creature1]
+'''
+
+adjective1 = input("An adjective\n >")
+pronoun1 = input("A pronoun\n >")
+adjective2 = input("Another adjective\n >")
+name1 = input("Any name\n >")
+adverb1 = input("An adverb\n >")
+verb1 = input("A verb\n >")
+creature1 = input("Any animal\n >")
+adjective3 =input("Yet another adjective\n >")
+verb2 = input("Another verb\n >")
+adverb2 = input("Another adverb\n >")
+
+
+print(f"There was once a person who lived in a {adjective1} spire. {pronoun1} was a very {adjective2} person, and their name was {name1}. They liked to do {adverb1} {verb1}. However, the {adjective2} person did have a {creature1}. And they liked to play with their {creature1}, but it was very {adjective3} and like to {verb2} very {adverb2}. So {name1} had to leave their {adjective1} spire in search of a better home for their {creature1}")
--- Practice/TomfooleryButActualStuff.py
+++ Practice/TomfooleryButActualStuff.py
@@ -4,4 +4,4 @@ where = input(f"Where is {favorite} found? \n> ")
 food = input(f"What do the like to eat?")
 
 print(f"Your favorite animal is {favorite} because they are {why}.")
-print(f"They live in {where} and like to eat {food}")
+print(f"They live in {where}, and like to eat {food}")
