a4d7cb4b9ff2412c20815690aa3d947f4393d396
Perry Henry <825834@stma.k12.mn.us>
2025-10-08 12:55:02 -0500

    Simple Madlib

---
+++ Practice/3.1 Bolean Expressions and Operators.py
@@ -0,0 +1,14 @@
+password = input("Enter password\n >")
+pin = input("Enter input\n >")
+#x = input("Number\n >")
+
+if password == "Henryiscool" or pin == "1234":
+    print("Password accepted")
+else:
+    print("Password Incorrect")
+'''
+if x<3 and x>2:
+    print("True")
+else:
+    print("False")
+'''
