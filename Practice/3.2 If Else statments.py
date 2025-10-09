fbe9689bd8d853642856e841609601cdc0823531
HenryPerry28 <825834@mystma.org>
2025-10-09 08:39:48 -0500

    Update 3.2 If Else statments.py

--- Practice/3.2 If Else statments.py
+++ Practice/3.2 If Else statments.py
@@ -1,15 +1,26 @@
-#password = input("Password:\n >")
-#pin = input("Password:\n >")
-#
-#if password == "Redapple25" and pin == "1025":
-#    print("Phase 1 passed")
-#    q1 = input("What is my first pets name?\n >")
-#    q2 = input("What is the species of my first pet?\n >")
-#    if q1 == "Zero" and q2 == "Dog" or "dog":
-#        print("Phase 2 passed")
-#else:
-#    print("Access Denied")
+password = input("Password:\n >")
+pin = input("Password:\n >")
+def password_wall ():
+    if password == "Redapple25" and pin == "2025":
+        print("Phase 1 passed")
+        q1 = input("What is my first pets name?\n >")
+        q2 = input("What is the species of my first pet?\n >")
+        if q1 == "Zero" and q2 == "Dog" or "dog":
+            print("Phase 2 passed")
+            q3 = input("What is the name of my favorite pet\n >")
+            q4 = input("How old is my favorite pet\n >")
+            if q3 == "Zen" and q4 == "7" or "7 years"
+            
+            else:
+                print("ACCESS DENIED")
+        else: 
+            print("ACCESS DENIED")
+    else:
+    print("ACCESS DENIED")
 
+password_wall():
+
+'''
 def password_return():
     x = input("What is the password?\n >")
     if x == "help":
@@ -19,3 +30,4 @@ def password_return():
         password_return()
 
 password_return()
+'''
