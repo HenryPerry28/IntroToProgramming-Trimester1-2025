5b8b4413cb08bc7344d4ab6b8603fb0511321bc4
HenryPerry28 <825834@mystma.org>
2025-10-21 19:48:06 -0500

    Update Text Adventure.py

--- Unit 2 Test/Text Adventure.py
+++ Unit 2 Test/Text Adventure.py
@@ -2,198 +2,144 @@ import random
 
 print("You will be going on an adventure, you will have a choice of certain paths to take, to choose the path, enter the number related to the choice.")
 
+def race_selection():
+    global final_race, race_choice
+    print("The first step in your adventure is to choose your race")
+    print("Race options are: Human, Elf, Dwarf or Tiefling.")
+    all_races = ["Human", "Elf", "Dwarf", "Tiefling"]
+    while True:
+        race_choice = input("Choose a race\n > ").title()
+        if not race_choice in all_races:
+            print("Choose one of the race options (an actual race)")
+        else:
+            break
+    final_race = subrace_selection() or race_choice
+    print(f"Your final race is {final_race}")
+    return final_race
+
+def subrace_selection():
+    if race_choice == "Elf":
+        all_subraces = ["High Elf", "Wood elf", "Drow",]
+        print("Elves have three different subraces: High elf, Wood elf, and Drow.")
+    elif race_choice == "Dwarf":
+        all_subraces = ["Mountain dwarf", "Hill dwarf"]
+        print("Dwarfs have two different subraces: Hill dwarf and Mountain dwarf.")
+    else:
+        return None
+    
+    while True:
+        final_subrace = input("Choose a subrace")
+        if not final_subrace in all_subraces:
+            print("Choose one of the options")
+        else: 
+            return final_subrace 
+    
+def race_stat_bonus():
+    global str_stat, dex_stat, con_stat, wis_stat, int_stat, cha_stat
+    if final_race == "Human":
+        str_stat += 1; dex_stat += 1; con_stat += 1; wis_stat += 1; int_stat += 1; cha_stat += 1
+    elif final_race == "High elf":
+        dex_stat += 2; int_stat += 1
+    elif final_race == "Wood elf":
+        dex_stat += 2; wis_stat += 1
+    elif final_race == "Drow":
+        dex_stat += 2; cha_stat += 1
+    elif final_race == "Hill dwarf":
+        con_stat += 2; wis_stat += 1
+    elif final_race == "Mountain dwarf":
+        con_stat += 2; str_stat += 2
+    elif final_race == "Tiefling":
+        cha_stat += 2; int_stat += 1
+    
+def class_selction():
+    global final_class, class_choice
+    print("It is now time to choose your class")
+    print("Class options are: Fighter, Wizard, Rouge or Cleric.")
+    all_classes = ["Fighter", "Wizard", "Rouge", "Cleric"]
+    while True:
+        class_choice = input("Choose a class\n > ").title()
+        if not class_choice in all_classes:
+            print("Choose one of the race options (an actual race)")
+        else:
+            break
+    final_class = subrace_selection() or class_choice
+    print(f"Your final class is {final_class}")
+    return final_class
+
+
 def stat_calc():
     x = [(random.randint(1, 6)), (random.randint(1, 6)), (random.randint(1, 6)), (random.randint(1, 6))]
     y = min(x)
     x.remove (y)
     z = sum(x)
     print(z)
+    return z
 
-def str_mod_calc():
-    global str_mod
-    if str_stat == 1:
-        str_mod = -5
-    elif str_stat == 2 or str_stat == 3:
-        str_mod = -4
-    elif str_stat == 4 or str_stat == 5:
-        str_mod = -3
-    elif str_stat == 6 or str_stat == 7:
-        str_mod = -2
-    elif str_stat == 8 or str_stat == 9:
-        str_mod = -1
-    elif str_stat == 10 or str_stat == 11:
-        str_mod = 0
-    elif str_stat == 12 or str_stat == 13:
-        str_mod = 1
-    elif str_stat == 14 or str_stat == 15:
-        str_mod = 2
-    elif str_stat == 16 or str_stat == 17:
-        str_mod = 3
-    elif str_stat == 18 or str_stat == 19:
-        str_mod = 4
-    elif str_stat == 20:
-        str_mod = 5
-    print(str_mod)
+def str_mod_calc(str_stat):
+    str_mod = (str_stat - 10) // 2
+    print(f"Your strength modifier is: {str_mod}")
 
-def str_dex_calc():
-    global dex_mod
-    if dex_stat == 1:
-        dex_mod = -5
-    elif dex_stat == 2 or dex_stat == 3:
-        dex_mod = -4
-    elif dex_stat == 4 or dec_stat == 5:
-        dex_mod = -3
-    elif dex_stat == 6 or dex_stat == 7:
-        dex_mod = -2
-    elif dex_stat == 8 or dex_stat == 9:
-        dex_mod = -1
-    elif dex_stat == 10 or dex_stat == 11:
-        dex_mod = 0
-    elif dex_stat == 12 or dex_stat == 13:
-        dex_mod = 1
-    elif dex_stat == 14 or dex_stat == 15:
-        dex_mod = 2
-    elif dex_stat == 16 or dex_stat == 17:
-        dex_mod = 3
-    elif dex_stat == 18 or dex_stat == 19:
-        dex_mod = 4
-    elif dex_stat == 20:
-        dex_mod = 5
-    print(dex_mod)
+def dex_mod_calc(dex_stat):
+    dex_mod = (dex_stat - 10) // 2
+    print(f"Your dexterity modifier is: {dex_mod}")
 
-def con_mod_calc():
-    global con_mod
-    if con_stat == 1:
-        con_mod = -5
-    elif con_stat == 2 or con_stat == 3:
-        con_mod = -4
-    elif con_stat == 4 or con_stat == 5:
-        con_mod = -3
-    elif con_stat == 6 or con_stat == 7:
-        con_mod = -2
-    elif con_stat == 8 or con_stat == 9:
-        con_mod = -1
-    elif con_stat == 10 or con_stat == 11:
-        con_mod = 0
-    elif con_stat == 12 or con_stat == 13:
-        con_mod = 1
-    elif con_stat == 14 or con_stat == 15:
-        con_mod = 2
-    elif con_stat == 16 or con_stat == 17:
-        con_mod = 3
-    elif con_stat == 18 or con_stat == 19:
-        con_mod = 4
-    elif con_stat == 20:
-        con_mod = 5
-    print(con_mod)
+def con_mod_calc(con_stat):
+    con_mod = (con_stat - 10) // 2
+    print(f"Your constitution modifier is: {con_mod}.")
 
-def wis_mod_calc():
-    global wis_mod
-    if wis_stat == 1:
-        wis_mod = -5
-    elif wis_stat == 2 or wis_stat == 3:
-        wis_mod = -4
-    elif wis_stat == 4 or wis_stat == 5:
-        wis_mod = -3
-    elif wis_stat == 6 or wis_stat == 7:
-        wis_mod = -2
-    elif wis_stat == 8 or wis_stat == 9:
-        wis_mod = -1
-    elif wis_stat == 10 or wis_stat == 11:
-        wis_mod = 0
-    elif wis_stat == 12 or wis_stat == 13:
-        wis_mod = 1
-    elif wis_stat == 14 or wis_stat == 15:
-        wis_mod = 2
-    elif wis_stat == 16 or wis_stat == 17:
-        wis_mod = 3
-    elif wis_stat == 18 or wis_stat == 19:
-        wis_mod = 4
-    elif wis_stat == 20:
-        wis_mod = 5
-    print(wis_mod)
+def wis_mod_calc(wis_stat):
+    wis_mod = (wis_stat - 10) // 2
+    print(f"Your wisdom modifier is: {wis_mod}")
 
-def int_mod_calc():
-    global int_mod
-    if int_stat == 1:
-        int_mod = -5
-    elif int_stat == 2 or int_stat == 3:
-        int_mod = -4
-    elif int_stat == 4 or int_stat == 5:
-        int_mod = -3
-    elif int_stat == 6 or int_stat == 7:
-        int_mod = -2
-    elif int_stat == 8 or int_stat == 9:
-        int_mod = -1
-    elif int_stat == 10 or int_stat == 11:
-        int_mod = 0
-    elif int_stat == 12 or int_stat == 13:
-        int_mod = 1
-    elif int_stat == 14 or int_stat == 15:
-        int_mod = 2
-    elif int_stat == 16 or int_stat == 17:
-        int_mod = 3
-    elif int_stat == 18 or int_stat == 19:
-        int_mod = 4
-    elif int_stat == 20:
-        int_mod = 5
-    print(int_mod)
+def int_mod_calc(int_stat):
+    int_mod = (int_stat - 10) // 2
+    print(f"Your intelligence modifier is: {int_mod}")
 
-def cha_mod_calc():
-    global cha_mod
-    if cha_stat == 1:
-        cha_mod = -5
-    elif cha_stat == 2 or cha_stat == 3:
-        cha_mod = -4
-    elif cha_stat == 4 or cha_stat == 5:
-        cha_mod = -3
-    elif cha_stat == 6 or cha_stat == 7:
-        cha_mod = -2
-    elif cha_stat == 8 or cha_stat == 9:
-        cha_mod = -1
-    elif cha_stat == 10 or cha_stat == 11:
-        cha_mod = 0
-    elif cha_stat == 12 or cha_stat == 13:
-        cha_mod = 1
-    elif cha_stat == 14 or cha_stat == 15:
-        cha_mod = 2
-    elif cha_stat == 16 or cha_stat == 17:
-        cha_mod = 3
-    elif cha_stat == 18 or cha_stat == 19:
-        cha_mod = 4
-    elif cha_stat == 20:
-        cha_mod = 5
-    print(cha_mod)
+def cha_mod_calc(cha_stat):
+    cha_mod = (cha_stat - 10) // 2
+    print(f"Your charisma modifier is: {cha_mod}")
 
 def all_stats_assign():
-    s1 = stat_calc()
-    s2 = stat_calc()
-    s3 = stat_calc()
-    s4 = stat_calc()
-    s5 = stat_calc()
-    s6 = stat_calc()
-    global all_stats_list
-    rand_stats_list= [s1, s2, s3, s4, s5, s6]
-    rand_stats_list.sort()
-    print("Now assign all number to a stat.")
-    global str_stat
-    str_stat = int(input("What is your strength base number?\n >"))
-    global dex_stat
-    dex_stat = int(input("What is your dexterity base number?\n >"))
-    global con_stat
-    con_stat = int(input("What is your constitution base number?\n >"))
-    global wis_stat
-    wis_stat = int(input("What is your wisdom base number?\n >"))
-    global int_stat
-    int_stat = int(input("What is your intelligence base number?\n >"))
-    global cha_stat
-    cha_stat = int(input("What is your charisma base number?\n >"))
-    player_stats_sorted_list = [str_stat, dex_stat, con_stat, wis_stat, int_stat, cha_stat]
-    if player_stats_sorted_list == rand_stats_list:
-        print("Right order")
-    else:
-        print("Wrong order")
+    try:
+        s1 = stat_calc()
+        s2 = stat_calc()
+        s3 = stat_calc()
+        s4 = stat_calc()
+        s5 = stat_calc()
+        s6 = stat_calc()
+        print("Now assign all numbers to a stat.")
+        global rand_stats_list
+        rand_stats_list = [s1, s2, s3, s4, s5, s6]
+        global str_stat
+        str_stat = int(input("What is your strength base number?\n >"))
+        global dex_stat
+        dex_stat = int(input("What is your dexterity base number?\n >"))
+        global con_stat
+        con_stat = int(input("What is your constitution base number?\n >"))
+        global wis_stat
+        wis_stat = int(input("What is your wisdom base number?\n >"))
+        global int_stat
+        int_stat = int(input("What is your intelligence base number?\n >"))
+        global cha_stat
+        cha_stat = int(input("What is your charisma base number?\n >"))
+        player_stats_sorted_list = [str_stat, dex_stat, con_stat, wis_stat, int_stat, cha_stat]
+        
+        race_stat_bonus()
+
+        if sorted(player_stats_sorted_list) == sorted(rand_stats_list):
+            str_mod_calc(str_stat)
+            dex_mod_calc(dex_stat)
+            con_mod_calc(con_stat)
+            wis_mod_calc(wis_stat)
+            int_mod_calc(int_stat)
+            cha_mod_calc(cha_stat)
+        else:
+            print("Enter only the numbers given, you bum!")
+    except ValueError:
+        print("Only integers, pal.")
     
+race_selection()
+
 all_stats_assign()
 
-str_mod_calc()
+class_selction()
