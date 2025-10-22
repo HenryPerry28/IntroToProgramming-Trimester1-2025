f282b24501dd6310e1070cc34707ca5ae456346c
HenryPerry28 <825834@mystma.org>
2025-10-22 12:51:58 -0500

    Update Text Adventure.py

--- Unit 2 Test/Text Adventure.py
+++ Unit 2 Test/Text Adventure.py
@@ -1,6 +1,22 @@
 import random
 
+global gold, silver, copper
+gold = 1
+sliver = 7 
+copper = 23
+
 print("You will be going on an adventure, you will have a choice of certain paths to take, to choose the path, enter the number related to the choice.")
+
+def roll_20():
+    return random.randint(1, 20)
+def roll_10():
+    return random.randint(1, 10)
+def roll_8():
+    return random.randint(1, 8)
+def roll_6
+    return random.randint(1, 6)
+def roll_4():
+    return random.randint(1, 4)
 
 def race_selection():
     global final_race, race_choice
@@ -52,20 +68,33 @@ def race_stat_bonus():
         cha_stat += 2; int_stat += 1
     
 def class_selction():
-    global final_class, class_choice
+    global class_choice
     print("It is now time to choose your class")
-    print("Class options are: Fighter, Wizard, Rouge or Cleric.")
-    all_classes = ["Fighter", "Wizard", "Rouge", "Cleric"]
+    print("Class options are: Fighter, Wizard, Rogue or Cleric.")
+    all_classes = ["Fighter", "Wizard", "Rogue", "Cleric"]
     while True:
         class_choice = input("Choose a class\n > ").title()
         if not class_choice in all_classes:
             print("Choose one of the race options (an actual race)")
         else:
             break
-    final_class = subrace_selection() or class_choice
-    print(f"Your final class is {final_class}")
-    return final_class
+    return class_choice
 
+def class_key_decider():
+    global class_key
+    if class_choice == "Fighter":
+        class_key = 1 
+        print(class_key)
+    elif class_choice == "Wizard":
+        class_key = 2
+        print(class_key)
+    elif class_choice == "Rogue":
+        class_key = 3
+        print(class_key)
+    elif class_choice == "Cleric
+        class_key = 4
+        print(class_key)
+    return class_key
 
 def stat_calc():
     x = [(random.randint(1, 6)), (random.randint(1, 6)), (random.randint(1, 6)), (random.randint(1, 6))]
@@ -134,12 +163,138 @@ def all_stats_assign():
             int_mod_calc(int_stat)
             cha_mod_calc(cha_stat)
         else:
-            print("Enter only the numbers given, you bum!")
+            print("Enter only the numbers given, you cheater!")
     except ValueError:
         print("Only integers, pal.")
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def orc_interaction():
+    if s1_c2 == "1":
+        if silver >= 10:
+            print("He takes your money and says, 'Great, I never need to see you again.'")
+            silver -= 10
+            return silver
+        elif (silver + (copper // 10)) >= silver_payment:
+            global inn_payment
+            inn_payment = 10
+            if silver <= inn_payment:
+                silver -= silver
+                x = (inn_payment - silver); copper - (x * 10) 
+            else:
+                silver -= inn_payment
+                print("He takes your money and saya, 'Great, now I never need to see you again'")
+        else:
+            print("'You don't have enough money, head to the bank if you wanna get that gold piece traded in for silver or copper.' He grunts.")
+    elif s1_c2 == "2":
+        print("I could lower the prices, but depends what's in it for me")
+        bargain_roll1 = (roll_20() + cha_mod)
+        if bargain_roll >= -5
+            print("You didn't do nothing for me, so why should I cut you any slack, just figure out a way to get me the money.")
+            orc_man_relation_npc -= 1
+        elif bargain_roll >= 5
+            print("I feel bad for you, but not enough to care, give me the money or go find it.")
+            orc_man_relation_npc += 0
+        elif bargain_roll >= 10
+            print("Just cause I feel bad for such a weak and pathetic adventurer, I will give you 3 silver off")
+            orc_man_relation_npc += 0
+            inn_payment -= 3
+        elif bargain_roll >= 15
+            print("'You make a point, you suck at life, I'll give you 5 off, you failure.' He says laughing jovially")
+            inn_payment -= 5; orc_man_relation_npc += 1
+    elif s1_c2 == "3": 
+        print("Good, find me by 12:00 today.")
+        orc_man_relation_npc += 0
+    elif s1_c2 == "4": 
+        print("Okay pal, the size between me and you alone makes me way stronger".) 
+        print("If you don't come up with the payments by 12:00, I'll find you with my squad.")
+        orc_man_relation_npc -= 2
+
+def story_start():
+    print("You wake up on your bed in the The Firewater Inn, you had rented it for the week.")
+    print("As you wake, their is a knock on your door.")
+    while True:
+        print("1. Answer the door")
+        print("2. Tell them to come in")
+        print("3. Ignore them")
+        s1_c1 = input("Choice (remember input just the number)\n >")
+        av_ch_s1_c1 = ["1", "2", "3"]
+        if not s1_c1 in av_ch_s1_c1:
+            print("Choose a valid choice")
+        else:
+            break
+    if s1_c1 == "1":
+        print("A large orcish man greets you, and says that payments is due today, and if you don't pay he will kick you out.")
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        orc_man_relation_npc = 0
+        while True:
+            print("What would you like to do?")
+            print(f"1. Pay the 10 silver (Gold: {gold}, Silver: {silver}, Copper: {copper}")
+            print("2. Discuss payment plans (barter)")
+            print("3. Agree to pay later")
+            print("4. Say you aren't paying, ever")
+            s1_c2 = input("Choice\n >")
+            av_ch_s1_c2 = ["1", "2", "3", "4"]
+            if not s1_c2 in av_ch_s1_c2:
+                print("Choose a valid choice (only the number)")
+            else:
+                break
+        if s1_c2 == "1":
+            if silver >= 10:
+                print("He takes your money and says, 'Great, I never need to see you again.'")
+                silver -= 10
+                return silver
+            elif (silver + (copper // 10)) >= silver_payment:
+                global inn_payment
+                inn_payment = 10
+                if silver <= inn_payment:
+                    silver -= silver
+                    x = (inn_payment - silver); copper - (x * 10) 
+                else:
+                    silver -= inn_payment
+                print("He takes your money and saya, 'Great, now I never need to see you again'")
+            else:
+                print("'You don't have enough money, head to the bank if you wanna get that gold piece traded in for silver or copper.' He grunts.")
+        elif s1_c2 == "2":
+            print("I could lower the prices, but depends what's in it for me")
+            bargain_roll1 = (roll_20() + cha_mod)
+            orc_man_relation_npc += 0
+            if bargain_roll >= -5
+                print("You didn't do nothing for me, so why should I cut you any slack, just figure out a way to get me the money.")
+                orc_man_relation_npc -= 1
+            elif bargain_roll >= 5
+                print("I feel bad for you, but not enough to care, give me the money or go find it.")
+                orc_man_relation_npc += 0
+            elif bargain_roll >= 10
+                print("Just cause I feel bad for such a weak and pathetic adventurer, I will give you 3 silver off")
+                orc_man_relation_npc += 0
+                inn_payment -= 3
+            elif bargain_roll >= 15
+                print("'You make a point, you suck at life, I'll give you 5 off, you failure.' He says laughing jovially")
+                inn_payment -= 5; orc_man_relation_npc += 1
+        elif s1_c2 == "3": 
+                print("Good, find me by 12:00 today.")
+                orc_man_relation_npc += 0
+        elif s1_c2 == "4": 
+                print("Okay pal, the size between me and you alone makes me way stronger".) 
+                print("If you don't come up with the payments by 12:00, I'll find you with my squad.")
+                orc_man_relation_npc -= 2
+    elif s1_c1 == "2"
+        print("A large orcish man opens a door, tells you that payments is due, and if you don't pay he will kick you out.")
+        orc_man_relation_npc += 0
+        
+
+    elif s1_c1 == "3"
+        print("'WAKE UP! Payment is due! I'm kicking you out if you don't pay by 12:00 today, and it's already 10:00.' Yells a large voice.")
+        orc_man_relation_npc -= 1
+        
     
 race_selection()
 
 all_stats_assign()
 
 class_selction()
+
+class_key_decider()
